b55e859ab866da13f05308de2a4357b0774fee01
Jahnvi Gangwar <59444913+jahnvigangwar@users.noreply.github.com>
2023-10-19 13:56:03 +0530

    Update RewardValueTests.java

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -19,12 +19,32 @@ public class RewardValueTests {
     }
 
     @Test
-    void convert_from_cash_to_miles() {
-        assert false;
+    public void testCashToMilesConversion() {
+        // Given a cash value of $100
+        double cashValue = 100.0;
+
+        // Create a RewardValue object
+        RewardValue rewardValue = new RewardValue(cashValue);
+
+        // Expected miles value should be calculated using the conversion rate (0.0035)
+        int expectedMilesValue = (int) (cashValue / 0.0035);
+
+        // Verify that the conversion from cash to miles is accurate
+        assertEquals(expectedMilesValue, rewardValue.getMilesValue());
     }
 
     @Test
-    void convert_from_miles_to_cash() {
-        assert false;
+    public void testMilesToCashConversion() {
+        // Given a miles value of 5000
+        int milesValue = 5000;
+
+        // Create a RewardValue object
+        RewardValue rewardValue = new RewardValue(milesValue);
+
+        // Expected cash value should be calculated using the conversion rate (0.0035)
+        double expectedCashValue = milesValue * 0.0035;
+
+        // Verify that the conversion from miles to cash is accurate
+        assertEquals(expectedCashValue, rewardValue.getCashValue(), 0.001); // Using delta for double comparison
     }
 }
